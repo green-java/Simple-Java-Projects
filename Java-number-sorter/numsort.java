253f3f707a3a73a732a00faaba7ec555119b0474
green-java <hellworlda@gmail.com>
2024-07-07 04:46:46 -0700

    java-number-sort are updated

---
+++ Java-number-sorter/numsort.java
@@ -0,0 +1,20 @@
+
+public class numsort {
+    public static void main(String[] args) {
+        int[] n={48,9,78,10,3,121};
+        int as=sort(n);
+        System.out.println(as);
+    }
+    static int sort(int[] num){
+        while(num>array.size(num))
+        {
+            int gr=num[0];
+            if(gr<num[1]){
+                gr=num[1];
+            }
+            num[1]+=1;
+
+        }
+        return 
+    }
+}
